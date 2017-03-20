7d7ea4fe333191b2ec8f97cf22494399498fe490
agn1 <ant.agn1@gmail.com>
2017-03-20 14:37:00 +0300

    change errors type filed to None

--- web/views.py
+++ web/views.py
@@ -136,7 +136,7 @@ class InstallView(AjaxableResponseMixin, FormView):
         def form_valid(self, form, *args, **kwargs):
             self.jsondata = form.installssl()
             if 'errors' in self.jsondata:
-                form.add_error('ERROR' ,self.jsondata['errors'])
+                form.add_error(None, self.jsondata['errors'])
                 return self.form_invalid(form)
             return super(InstallView, self).form_valid(form)
 
